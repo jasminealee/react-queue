42e32eb27f9c547ffff797e3f3a623affe32604d
Student <example@example.com>
2019-05-08 16:36:51 -0700

    add more

--- _tests_/reducers/ticket-list-reducer.test.js
+++ _tests_/reducers/ticket-list-reducer.test.js
@@ -11,27 +11,21 @@ describe('ticketListReducer', () => {
     id: 0
   };
 
-  test('Should return default state if no action type is recognized', () => {
-    expect(ticketListReducer({}, { type: null })).toEqual({});
-  });
-
-  test('Should successfully add new ticket data to masterTicketList', () => {
+  test('Should add freshly-calculated Moment-formatted wait time to ticket entry', () => {
     const { names, location, issue, timeOpen, id } = sampleTicketData;
     action = {
-      type: 'ADD_TICKET',
-      names: names,
-      location: location,
-      issue: issue,
-      timeOpen: timeOpen,
+      type: 'UPDATE_TIME',
+      formattedWaitTime: '4 minutes',
       id: id
     };
-    expect(ticketListReducer({}, action)).toEqual({
+    expect(ticketListReducer({ [id] : sampleTicketData }, action)).toEqual({
       [id] : {
         names: names,
         location: location,
         issue: issue,
         timeOpen: timeOpen,
-        id: id
+        id: id,
+        formattedWaitTime: '4 minutes'
       }
     });
   });
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
@@ -11,17 +12,9 @@ import PropTypes from 'prop-types';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTicket: null
-    };
-    this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
-  }
-
   componentDidMount() {
     this.waitTimeUpdateTimer = setInterval(() =>
-      this.updateTicketElapsedWaitTime(),
+    this.updateTicketElapsedWaitTime(),
     60000
     );
   }
@@ -31,15 +24,17 @@ class App extends React.Component {
   }
 
   updateTicketElapsedWaitTime() {
-    // var newMasterTicketList = Object.assign({}, this.state.masterTicketList);
-    // Object.keys(newMasterTicketList).forEach(ticketId => {
-    //   newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
-    // });
-    // this.setState({masterTicketList: newMasterTicketList});
-  }
-
-  handleChangingSelectedTicket(ticketId){
-    this.setState({selectedTicket: ticketId});
+    const { dispatch } = this.props;
+    Object.keys(this.props.masterTicketList).map(ticketId => {
+      const ticket = this.props.masterTicketList[ticketId];
+      const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
+      const action = {
+        type: 'UPDATE_TIME',
+        id: ticketId,
+        formattedWaitTime: newFormattedWaitTime
+      };
+      dispatch(action);
+    });
   }
 
   render(){
@@ -49,9 +44,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' render={()=><TicketList ticketList={this.props.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl />} />
-          <Route path='/admin' render={(props)=><Admin ticketList={this.props.masterTicketList} currentRouterPath={props.location.pathname}
-            onTicketSelection={this.handleChangingSelectedTicket}
-            selectedTicket={this.state.selectedTicket}/>} />
+          <Route path='/admin' render={(props)=><Admin currentRouterPath={props.location.pathname} />} />
           <Route component={Error404} />
         </Switch>
       </div>
@@ -59,14 +52,14 @@ class App extends React.Component {
   }
 }
 
-App.propTypes = {
-  masterTicketList: PropTypes.object
-};
-
-const mapStateToProps = state => {
-  return {
-    masterTicketList: state.masterTicketList
+  App.propTypes = {
+    masterTicketList: PropTypes.object
   };
-};
+
+  const mapStateToProps = state => {
+    return {
+      masterTicketList: state.masterTicketList
+    };
+  };
 
 export default withRouter(connect(mapStateToProps)(App));
